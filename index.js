156d194240e2c2a2e7b32aa3d82a3cf1b6c98cfb
unknown <724804113@qq.com>
2026-02-23 16:28:07 +0800

    修复

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-let headers = {
-	'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ja;q=0.5',
-	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0'
-}
-
 const userConfig = window.Rulia.getUserConfig()
 
 let authorization = false;
@@ -14,37 +9,40 @@ if (userConfig.authorization) {
 async function setMangaListFilterOptions() {
 	const url = 'https://api.copy2000.online/api/v3/h5/filter/comic/tags';
 	try {
+
 		let result = [{
-				label: '主题',
-				name: 'theme',
-				options: [{
-					label: '推荐',
-					value: 'recommend'
-				}, {
-					label: '全部',
-					value: 0
-				}]
-			},
-			{
-				label: '地区/进度',
-				name: 'top',
-				options: [{
-					label: '全部',
-					value: 0
-				}]
-			}
-		]
+			label: '主题',
+			name: 'theme',
+			options: [{
+				label: '推荐',
+				value: 'recommend'
+			}, {
+				label: '全部',
+				value: 0
+			}]
+		}]
+
 		if (authorization) {
 			result[0].options.push({
 				label: '书架',
 				value: 'bookshelf'
 			})
 		}
+
 		const rawResponse = await window.Rulia.httpRequest({
 			url: url,
 			method: 'GET',
 			payload: 'format=json&type=1',
-			headers: headers
+			headers: {
+				'Accept': 'application/json',
+				'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+				'Origin': 'https://m.relamanhua.org',
+				'Version': '2025.11.21',
+				'Webp': '1',
+				'platform': '1',
+				'Host': 'mapi.hotmangasd.com',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+			}
 		});
 		const response = JSON.parse(rawResponse);
 		for (let item of response.results.theme) {
@@ -53,28 +51,70 @@ async function setMangaListFilterOptions() {
 				value: item.path_word
 			})
 		}
-		for (let item of response.results.top) {
-			result[1].options.push({
-				label: item.name,
-				value: item.path_word
-			})
-		}
 		window.Rulia.endWithResult(result);
 	} catch (error) {
 		window.Rulia.endWithResult([])
+	}
+
+}
+
+async function getMangaListBySearching(page, pageSize, keyword) {
+	const url =
+		'https://mapi.hotmangasd.com/api/v3/search/comic';
+	try {
+		const rawResponse = await window.Rulia.httpRequest({
+			url: url,
+			method: 'GET',
+			payload: 'limit=' + pageSize + '&offset=' + ((page - 1) * pageSize) +
+				'&platform=2&q=' + keyword + '&q_type=',
+			headers: {
+				'Accept': 'application/json',
+				'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+				'Origin': 'https://m.relamanhua.org',
+				'Version': '2025.11.21',
+				'Webp': '1',
+				'platform': '1',
+				'Host': 'mapi.hotmangasd.com',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+			}
+		});
+		const response = JSON.parse(rawResponse);
+		let result = {
+			list: []
+		}
+		for (let manga of response.results.list) {
+			let comic = {
+				title: manga.name,
+				url: 'https://www.mangacopy.com/comic/' + manga.path_word,
+				coverUrl: manga.cover
+			}
+			result.list.push(comic);
+		}
+		window.Rulia.endWithResult(result);
+	} catch (error) {
+		window.Rulia.endWithException(error.message);
 	}
 }
 
 async function getMangaListByRecommend(page, pageSize) {
 	const url =
-		'https://api.copy2000.online/api/v3/recs';
+		'https://mapi.hotmangasd.com/api/v3/recs';
 	try {
 		const rawResponse = await window.Rulia.httpRequest({
 			url: url,
 			method: 'GET',
 			payload: 'format=json&limit=' + pageSize + '&offset=' + ((page - 1) * pageSize) +
 				'&pos=3200102',
-			headers: headers
+			headers: {
+				'Accept': 'application/json',
+				'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+				'Origin': 'https://m.relamanhua.org',
+				'Version': '2025.11.21',
+				'Webp': '1',
+				'platform': '1',
+				'Host': 'mapi.hotmangasd.com',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+			}
 		});
 		const response = JSON.parse(rawResponse);
 		let result = {
@@ -96,7 +136,7 @@ async function getMangaListByRecommend(page, pageSize) {
 
 async function getMangaListByBookshelf(page, pageSize) {
 	const url =
-		'https://api.copy2000.online/api/v3/member/collect/comics';
+		'https://www.mangacopy.com/api/v3/member/collect/comics';
 	try {
 		const rawResponse = await window.Rulia.httpRequest({
 			url: url,
@@ -134,23 +174,28 @@ async function getMangaListByCategory(page, pageSize, filterOptions) {
 	} else if (filterOptions.theme == 'bookshelf') {
 		return await getMangaListByBookshelf(page, pageSize);
 	} else {
-		const url = 'https://api.copy2000.online/api/v3/comics';
+		const url = 'https://mapi.hotmangasd.com/api/v3/comics';
 		try {
 			let theme = '';
-			let top = '';
 			if (filterOptions.theme && filterOptions.theme != 0) {
 				theme = '&theme=' + filterOptions.theme;
 			}
-			if (filterOptions.top && filterOptions.top != 0) {
-				top = '&top=' + filterOptions.top;
-			}
 			let payload = '_update=true&format=json&free_type=1&limit=' + pageSize + '&offset=' + ((page - 1) *
-				pageSize) + '&ordering=popular' + theme + top;
+				pageSize) + '&ordering=popular' + theme;
 			const rawResponse = await window.Rulia.httpRequest({
 				url: url,
 				method: 'GET',
 				payload: payload,
-				headers: headers
+				headers: {
+					'Accept': 'application/json',
+					'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+					'Origin': 'https://m.relamanhua.org',
+					'Version': '2025.11.21',
+					'Webp': '1',
+					'platform': '1',
+					'Host': 'mapi.hotmangasd.com',
+					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+				}
 			})
 			const response = JSON.parse(rawResponse);
 			let result = {
@@ -171,37 +216,6 @@ async function getMangaListByCategory(page, pageSize, filterOptions) {
 	}
 }
 
-async function getMangaListBySearching(page, pageSize, keyword) {
-	const url =
-		'https://api.copy2000.online/api/kb/web/searcha/comics';
-	try {
-		const rawResponse = await window.Rulia.httpRequest({
-			url: url,
-			method: 'GET',
-			payload: 'limit=' + pageSize + '&offset=' + ((page - 1) * pageSize) +
-				'&platform=2&q=' + keyword + '&q_type=',
-			headers: {
-				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0'
-			}
-		});
-		const response = JSON.parse(rawResponse);
-		let result = {
-			list: []
-		}
-		for (let manga of response.results.list) {
-			let comic = {
-				title: manga.name,
-				url: 'https://www.mangacopy.com/comic/' + manga.path_word,
-				coverUrl: manga.cover
-			}
-			result.list.push(comic);
-		}
-		window.Rulia.endWithResult(result);
-	} catch (error) {
-		window.Rulia.endWithException(error.message);
-	}
-}
-
 async function getMangaList(page, pageSize, keyword, rawFilterOptions) {
 	if (keyword) {
 		return await getMangaListBySearching(page, pageSize, keyword);
@@ -213,15 +227,24 @@ async function getMangaList(page, pageSize, keyword, rawFilterOptions) {
 async function getMangaData(dataPageUrl) {
 	const seasonIdMatchExp = /\/comic\/(.*)/;
 	const seasonIdMatch = dataPageUrl.match(seasonIdMatchExp);
-	const detailUrl = 'https://api.copy2000.online/api/v3/comic2/' + seasonIdMatch[1];
-	const chapterListUrl = 'https://api.copy2000.online/api/v3/comic/' + seasonIdMatch[1] +
+	const detailUrl = 'https://mapi.hotmangasd.com/api/v3/comic2/' + seasonIdMatch[1];
+	const chapterListUrl = 'https://mapi.hotmangasd.com/api/v3/comic/' + seasonIdMatch[1] +
 		'/group/default/chapters';
 	try {
 		const detailRawResponse = await window.Rulia.httpRequest({
 			url: detailUrl,
 			method: 'GET',
-			payload: 'format=json&platform=3',
-			headers: headers
+			payload: 'format=json',
+			headers: {
+				'Accept': 'application/json',
+				'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+				'Origin': 'https://m.relamanhua.org',
+				'Version': '2025.11.21',
+				'Webp': '1',
+				'platform': '1',
+				'Host': 'mapi.hotmangasd.com',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+			}
 		})
 		const detailResponse = JSON.parse(detailRawResponse);
 		let result = {
@@ -234,7 +257,16 @@ async function getMangaData(dataPageUrl) {
 			url: chapterListUrl,
 			method: 'GET',
 			payload: '_update=true&format=json&limit=500&offset=0',
-			headers: headers
+			headers: {
+				'Accept': 'application/json',
+				'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+				'Origin': 'https://m.relamanhua.org',
+				'Version': '2025.11.21',
+				'Webp': '1',
+				'platform': '1',
+				'Host': 'mapi.hotmangasd.com',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+			}
 		});
 		const chapterListResponse = JSON.parse(chapterListRawResponse);
 
@@ -254,29 +286,43 @@ async function getMangaData(dataPageUrl) {
 async function getChapterImageList(chapterUrl) {
 	const episodeIdMatchExp = /https?:\/\/www\.mangacopy\.com\/comic\/([a-zA-Z0-9_-]+)\/chapter\/([0-9a-f-]+)/;
 	const episodeIdMatch = chapterUrl.match(episodeIdMatchExp);
-	const url = 'https://api.copy2000.online/api/v3/comic/' + episodeIdMatch[1] + '/chapter2/' + episodeIdMatch[2];
+	const url = 'https://mapi.hotmangasd.com/api/v3/comic/' + episodeIdMatch[1] + '/chapter/' + episodeIdMatch[2];
 	const rawResponse = await window.Rulia.httpRequest({
 		url: url,
 		method: 'GET',
 		payload: 'format=json',
-		headers: headers
+		headers: {
+			'Accept': 'application/json',
+			'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+			'Origin': 'https://m.relamanhua.org',
+			'Version': '2025.11.21',
+			'Webp': '1',
+			'platform': '1',
+			'Host': 'mapi.hotmangasd.com',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+		}
 	});
 	const response = JSON.parse(rawResponse);
-	let result = [];
-	for (let i = 0; i < response.results.chapter.words.length; i++) {
-		result.push({
-			url: response.results.chapter.contents[i].url.replace(/c800x/, 'c1500x'),
-			index: response.results.chapter.words[i],
-			width: 1,
-			height: 1
-		});
+	let pageUrls = [];
+
+	for (let item of response.results.chapter.contents) {
+		pageUrls.push(item.url)
 	}
-	result.sort(function(a, b) {
-		return a.index - b.index;
-	});
-	for (let i = 0; i < result.length; i++) {
-		delete result[i].index;
-	}
+
+	const promises = pageUrls.map(url =>
+		new Promise(resolve => {
+			const img = new Image();
+			img.onload = img.onerror = () => {
+				resolve({
+					url,
+					width: img.width,
+					height: img.height
+				});
+			};
+			img.src = url;
+		})
+	);
+	const result = await Promise.all(promises);
 	window.Rulia.endWithResult(result);
 }
 
